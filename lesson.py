9ac555853d1ccccaeb917dccf70790981a20b7b0
haruspring-jokt <h0534jokt@gmail.com>
2018-11-13 00:05:21 +0900

    first commit

---
+++ lesson.py
@@ -0,0 +1,20 @@
+num_tuple = (10, 20)
+print(num_tuple)
+
+x, y = num_tuple
+print(x, y)
+
+x, y = (10, 30)
+print(x, y)
+
+i = 10
+j = 20
+tmp = i
+i = j
+j = tmp
+
+a = 100
+b = 200
+print(a, b)
+a, b = b, a
+print(a, b)
