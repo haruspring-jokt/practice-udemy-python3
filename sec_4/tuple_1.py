35e516f8d591a0279305d5cd417ce4d27c28bb6f
haruspring-jokt <h0534jokt@gmail.com>
2018-11-13 00:15:14 +0900

    finished tuple lesson

--- sec_4/tuple_1.py
+++ sec_4/tuple_1.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 num_tuple = (10, 20)
 print(num_tuple)
 
---
+++ sec_4/tuple_2.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+# d = {'x': 10, 'y': 20}
+# print(type(d))
+#
+# print(d['x'])
+# print(d['y'])
+#
+# d['x'] = 100
+#
+# print(d)
+
+chose_from_two = ('A', 'B', 'C')
+
+answer = []
+answer.append('A')
+answer.append('C')
+
+print(chose_from_two)
+print(answer)
